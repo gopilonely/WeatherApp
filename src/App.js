6d67b8d17c542a73971872e35feaa8700f13007a
A H GOPI <59681009+gopilonely@users.noreply.github.com>
2024-01-24 11:13:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import WeatherApp from './components/WeatherApp/WeatherApp.jsx';
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+      {/* <humidity-percent />
+      <wind_icon /> */}
+      {/* <cloud_icon />
+      <clear_icon />
+      <drizzle_icon />
+      <rain_icon />
+      <snow_icon /> */}
+
+    </div>
+  );
+}
+
+export default App;
